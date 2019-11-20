9f56ebe72b16b19be13aa0cdbeb2c5c92d8206fa
Joe <jodeph.broughton@gmail.com>
2019-11-20 16:06:49 +0000

    - Fixed the mess I made last commit by removing all the fish prefabs.

---
+++ CreativityCardsProject/Assets/Scripts/CaptureFish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureFish : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Fish")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
+
---
+++ CreativityCardsProject/Assets/Scripts/SlowFish.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowFish : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Projectile")
+        {
+            gameObject.GetComponent<FishMoving>().speed = 0;
+        }
+    }
+
+}
+
