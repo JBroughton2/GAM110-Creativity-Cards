7a443c124027612dde20028e075a9be440e9c6b8
Joe <jodeph.broughton@gmail.com>
2019-11-20 19:42:03 +0000

    Added walking and jumping animations

---
+++ Character Sprites/Character Sprites/READMEH.txt
@@ -0,0 +1,2 @@
+Contains the standing, running, and jumping sprites; lemme know if there's anything to be changed ASAP
+((again, be sure to format the sprite to "RGBA 32" to prevent colour fuckery))
--- CreativityCardsProject/Assets/Scripts/PlayerMovement.cs
+++ CreativityCardsProject/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float jumpY;
     public bool hasJumped = false;
+    private bool facingRight;
+    public Animator animator;
 
     private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        facingRight = true;
     }
 
     private void Update()
@@ -20,12 +23,15 @@ public class PlayerMovement : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector2 jump = new Vector2(0, jumpY);
 
+        animator.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+
         transform.position = new Vector2(transform.position.x + moveHorizontal, transform.position.y);
 
         if (Input.GetKeyDown(KeyCode.Space) && (!hasJumped))
         {
             hasJumped = true;
             rb2d.AddForce(jump, ForceMode2D.Impulse);
+            animator.SetBool("IsJumping", true);
         }
 
     }
@@ -35,6 +41,20 @@ public class PlayerMovement : MonoBehaviour
         if (other.gameObject.CompareTag("Floor"))
         {
             hasJumped = false;
+        }
+        animator.SetBool("IsJumping", false);
+    }
+
+    private void Flip(float moveHorizontal)   //don't uncomment this unless you want to flip and fix the gun sprite as well
+    {
+        if (moveHorizontal > 0 && !facingRight || moveHorizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
         }
     }
 
